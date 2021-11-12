02951c276ca09c0b8ae8fd0b6ba5c4d2616440a9
alon <alonspinner@gmail.com>
2021-11-12 21:38:23 +0200

    Finished HW2

--- HW/HW2/HW2q5.m
+++ HW/HW2/HW2q5.m
@@ -1,41 +1,41 @@
+%% Init
 [A,b] = ODEmodel(20);
 
 D = diag(diag(A));
 L = -tril(A,-1);
 U = -triu(A,1);
 
-
 % Jacobi spectral radius
-T = D\(L+U);
-C = D\b;
+G = D\(L+U);
+c = D\b;
 
-Lambda = max(abs(eig(T)))
+Lambda = max(abs(eig(G)));
 
-% 5.2
+errors_x = 10.^-(1:0.1:7);
+
+%% 5.2
 x0 = zeros(20,1);
-estimated_k = [];
-for i = 1:0.1:7
-    num_iterations = cal_iterations(T,C,x0,10^-i);
-    estimated_k = [estimated_k;num_iterations];
+estimated_k = zeros(size(errors_x));
+for ii = 1:length(errors_x)
+    estimated_k(ii) = itr_est(x0,G,c,errors_x(ii));
 end
 
 figure(1)
-plot(1:0.1:7,estimated_k,'o-','LineWidth',2);
+plot(log10(errors_x),estimated_k,'o-','LineWidth',2);
 ylabel('$$Estimated\;iterations$$','interpreter','latex');
-xlabel('$$Error$$','interpreter','latex');
+xlabel('$$log(Error Bound)$$','interpreter','latex');
 grid on;
 grid minor;
 
-norm_error = [];
-for i = 1:61
-    sol = iterative_solver(T,C,x0,round(estimated_k(i)));
-    norm_error = [norm_error,norm(A*sol-b,inf)];
+norm_error = zeros(size(errors_x));
+for ii = 1:61
+    sol = iterative_solver(G,c,x0,round(estimated_k(ii)));
+    norm_error(ii) = norm(A*sol-b,inf);
 end
 figure(2)
-errors_x = 10.^-(1:0.1:7);
 plot(errors_x,norm_error,'o')
 ylabel('$$Error$$','interpreter','latex');
-xlabel('$$Boundery$$','interpreter','latex');
+xlabel('$$Error Bound$$','interpreter','latex');
 grid on;
 grid minor;
 
