da98c8aa011a6e0b26419ccdaeb81c824f63c41f
alon <alonspinner@gmail.com>
2021-11-20 23:12:06 +0200

    Finished Writing HW3 code

--- HW/HW3/HW3.m
+++ HW/HW3/HW3.m
@@ -8,91 +8,62 @@ ptargets = [p1,p2,p3];
 errBound = 1e-4;
 [~,fP] = funcbund.symForwardKinematics();
 [~,fJ] = funcbund.symJacobian();
-%% Q1
+%% Q1.1
 N=20;
 [q1,q2,q3] = deal(linspace(-pi,pi,N));
-% solutions = findSolutions(q1,q2,q3,ptargets,fP,errBound);
-% x0Points = findx0Points4Solutions(q1,q2,q3,solutions,ptargets,fP,errBound);
-PlotSolutions(solutions,x0Points);
-%% Q1
-function solutions = findSolutions(q1,q2,q3,ptargets,fP,errBound)
-
-fsolveOptions = optimset('Display','off');
-N = length(q1);
-ntargets = size(ptargets,2);
-solutions = cell(1,ntargets);
-for mm = 1:ntargets
-    xsols = cell(N,N,N);
-    fPtarget = @(q) fP(q) - ptargets(:,mm);
-    for ii = 1:N
-        for jj = 1:N
-            for kk = 1:N
-                 x0 = [q1(ii),q2(jj),q3(kk)]';
-                 xsols{ii,jj,kk} = wrapToPi(fsolve(fPtarget,x0,fsolveOptions));
-                 disp(kk+(jj-1)*N+(ii-1)*N^2); %iteration count
-            end
+x0s = zeros(3,N^3);
+for ii = 1:N
+    for jj = 1:N
+        for kk = 1:N
+            x0s(:,kk+(jj-1)*N+(ii-1)*N^2) = [q1(ii),q2(jj),q3(kk)]';
         end
     end
-    xsols_goodIdx = cellfun(@(x) vecnorm(fPtarget(x),2)<errBound,xsols); %find solutions that pass criteria
-    xsols_good = cell2mat(xsols(xsols_goodIdx)')';%convert to Mx3 matrix
-    solutions{mm} = unique(round(xsols_good,4),'rows')'; %publish unique solutions
-end
 end
 
-function x0Points = findx0Points4Solutions(q1,q2,q3,solutions,ptargets,fP,errBound)
-fsolveOptions = optimset('Display','off');
-N = length(q1);
-ntargets = size(ptargets,2);
-x0Points=cell(1,3);
-for mm = 1:ntargets
-    fPtarget = @(q) fP(q) - ptargets(:,mm);
-    
-    %initalize
-    if isempty(solutions{mm}), continue; end
-    solAmnt = size(solutions{mm},2);
-    x0Points_mm = cell(1,solAmnt);
-    for ii=1:solAmnt
-        x0Points_mm{ii} = solutions{mm}(:,ii);
-    end
-    
-    %assign x0 points to each solution by distance of x from solution
-    for ii = 1:N
-        for jj = 1:N
-            for kk = 1:N
-                 x0 = [q1(ii),q2(jj),q3(kk)]';
-                 x = wrapToPi(fsolve(fPtarget,x0,fsolveOptions));
-                 if vecnorm(fPtarget(x),2)<errBound
-                    [~,minIdx]=min(vecnorm(x-solutions{mm}));
-                    x0Points_mm{minIdx}(:,end+1) = x0;
-                 end
-                 disp(kk+(jj-1)*N+(ii-1)*N^2); %iteration count
-            end
+solutions = funcbund.findSolutions(x0s,ptargets,fP,errBound);
+x0Points = funcbund.findx0Points4Solutions(x0s,solutions,ptargets,fP,errBound);
+funcbund.PlotSolutions(solutions,x0Points);
+
+%% Q1.2
+N=3;
+[q1,q2,q3] = deal(linspace(-pi,pi,N));
+x0s = zeros(3,N^3);
+for ii = 1:N
+    for jj = 1:N
+        for kk = 1:N
+            x0s(:,kk+(jj-1)*N+(ii-1)*N^2) = [q1(ii),q2(jj),q3(kk)]';
         end
     end
-    
-    x0Points{mm} = x0Points_mm;
 end
-end
-%% Q1 plot
-function PlotSolutions(solutions,x0Points)
-for mm=1:length(x0Points)
-    if isempty(solutions{mm}), continue; end
-    
-    fig=figure;
-    ax=axes(fig);
-    grid(ax,'on'); hold(ax,'on');
-    xlabel(ax,'$$\Theta_{1}$$','Interpreter','latex');
-    ylabel(ax,'$$\Theta_{2}$$','Interpreter','latex');
-    zlabel(ax,'$$\Theta_{3}$$','Interpreter','latex');
-    view(ax,3);
-    
-    solAmnt = size(solutions{mm},2);
-    color = lines(solAmnt);
-    for ii=1:solAmnt
-        XYZ = x0Points{mm}{ii}';
-        scatter3(ax,XYZ(1,1),XYZ(2,2),XYZ(3,3),200,color(ii,:),'filled','diamond');
-        scatter3(ax,XYZ(2:end,1),XYZ(2:end,2),XYZ(2:end,3),20,color(ii,:),'filled',...
-            'MarkerEdgeColor','none','MarkerFaceAlpha',0.5);
+
+solutions = funcbund.findSolutions(x0s,ptargets,fP,errBound);
+x0Points = funcbund.findx0Points4Solutions(x0s,solutions,ptargets,fP,errBound);
+funcbund.PlotSolutions(solutions,x0Points);
+
+%% Q1.3
+N=3^3;
+x0s = wrapToPi(2*pi*rand(3,N));
+
+solutions = funcbund.findSolutions(x0s,ptargets,fP,errBound);
+x0Points = funcbund.findx0Points4Solutions(x0s,solutions,ptargets,fP,errBound);
+funcbund.PlotSolutions(solutions,x0Points);
+
+%% Q2.1
+J = funcbund.symJacobian();
+pretty(J);
+
+%% Q2.2
+N=20;
+[q1,q2,q3] = deal(linspace(-pi,pi,N));
+x0s = zeros(3,N^3);
+for ii = 1:N
+    for jj = 1:N
+        for kk = 1:N
+            x0s(:,kk+(jj-1)*N+(ii-1)*N^2) = [q1(ii),q2(jj),q3(kk)]';
+        end
     end
 end
-end
+
+solutions = funcbund.findSolutions(x0s,p1,fP,errBound,fJ);
+x0Points = funcbund.findx0Points4Solutions(x0s,solutions,p1,fP,errBound);
+funcbund.PlotSolutions(solutions,x0Points);
--- HW/HW3/funcbund.m
+++ HW/HW3/funcbund.m
@@ -23,7 +23,6 @@ classdef funcbund
             fP = matlabFunction(P,'vars',{[th1,th2,th3]'});
 
         end
-
         function [J,fJ] = symJacobian()
             P = funcbund.symForwardKinematics();
             q = symvar(P);
@@ -32,8 +31,103 @@ classdef funcbund
                 for jj=1:size(J,2)
                     J(ii,jj) = diff(P(ii),q(jj));
                 end
-            end 
+            end
             fJ = matlabFunction(J,'vars',{[q(1),q(2),q(3)]'});
+        end
+        function solutions = findSolutions(x0s,ptargets,fP,errBound,fJ)
+            ntargets = size(ptargets,2);
+            
+            if nargin > 4 %jacobian was provided
+                Jflag=true;
+                fsolveOptions = optimoptions('fsolve','SpecifyObjectiveGradient',true,'Display','off');
+            else
+                Jflag=false;
+                fsolveOptions = optimoptions('Display','off');
+            end
+            
+            solutions = cell(1,ntargets);
+            for mm = 1:ntargets
+                fPtarget = @(q) fP(q) - ptargets(:,mm);
+                if Jflag
+                    fPsoltarget = @(q) deal(fP(q) - ptargets(:,mm),fJ(q));
+                else
+                    fPsoltarget = fPtarget;
+                end
+                
+                xsols=zeros(size(x0s));
+                for ii=1:size(x0s,2)
+                    xsols(:,ii) = wrapToPi(fsolve(fPsoltarget,x0s(:,ii),fsolveOptions));
+                    disp(ii)
+                end
+                xsols_goodIdx = vecnorm(fPtarget(xsols),2,1)<errBound; %find solutions that pass criteria
+                xsols_good = xsols(:,xsols_goodIdx);
+                solutions{mm} = unique(round(xsols_good',4),'rows')'; %publish unique solutions
+            end
+            disp('finished finding solutions')
+        end
+        function x0Points = findx0Points4Solutions(x0s,solutions,ptargets,fP,errBound)
+            fsolveOptions = optimset('Display','off');
+            ntargets = size(ptargets,2);
+            x0Points=cell(1,ntargets);
+            for mm = 1:ntargets
+                fPtarget = @(q) fP(q) - ptargets(:,mm);
+
+                %initalize
+                %if isempty(solutions{mm}), continue; end
+                solAmnt = size(solutions{mm},2);
+                x0Points_mm = cell(1,solAmnt+1); %+1 for no solution points
+                for ii=1:solAmnt
+                    x0Points_mm{ii} = solutions{mm}(:,ii);
+                end
+
+                %assign x0 points to each solution by distance of x from solution
+                for ii=1:size(x0s,2)
+                    x0 = x0s(:,ii);
+                    x = wrapToPi(fsolve(fPtarget,x0,fsolveOptions));
+                    if vecnorm(fPtarget(x),2)<errBound
+                        [~,minIdx]=min(vecnorm(x-solutions{mm}));
+                        x0Points_mm{minIdx}(:,end+1) = x0;
+                    else
+                        x0Points_mm{end}(:,end+1) = x0;
+                    end
+
+                    disp(ii)
+                end
+
+                x0Points{mm} = x0Points_mm;
+                disp('finished assigning each x0 point to a solution')
+            end
+        end
+        function PlotSolutions(solutions,x0Points)
+            for mm=1:length(x0Points)
+                %if isempty(solutions{mm}), continue; end
+
+                fig=figure;
+                ax=axes(fig);
+                grid(ax,'on'); hold(ax,'on');
+                xlabel(ax,'$$\Theta_{1}$$','Interpreter','latex');
+                ylabel(ax,'$$\Theta_{2}$$','Interpreter','latex');
+                zlabel(ax,'$$\Theta_{3}$$','Interpreter','latex');
+                view(ax,3);
+                
+                %plot solutions
+                solAmnt = size(solutions{mm},2);
+                color = jet(solAmnt);
+                for ii=1:solAmnt
+                    XYZ = x0Points{mm}{ii}';
+                    scatter3(ax,XYZ(1,1),XYZ(1,2),XYZ(1,3),200,color(ii,:),'filled','diamond',...
+                        'MarkerEdgeColor',[0,0,0],'LineWidth',2);
+                    scatter3(ax,XYZ(2:end,1),XYZ(2:end,2),XYZ(2:end,3),20,color(ii,:),'filled',...
+                        'MarkerEdgeColor','none','MarkerFaceAlpha',0.5);
+                end
+
+                %plot not solutions
+                XYZ = x0Points{mm}{end}';
+                if ~isempty(XYZ)
+                    scatter3(ax,XYZ(:,1),XYZ(:,2),XYZ(:,3),3,[0 0 0],"filled",...
+                        'MarkerEdgeColor','none','MarkerFaceAlpha',0.5);
+                end
+            end
         end
     end
 end
