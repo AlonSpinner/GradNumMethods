d6411a00ff5d6a171727275ff6d8465faa0e0b7c
alon <alonspinner@gmail.com>
2022-01-06 19:38:42 +0200

    Added plots, removed outputfcn globals

--- HW/HW6/funcbund.m
+++ HW/HW6/funcbund.m
@@ -2,7 +2,7 @@ classdef funcbund
     properties(Constant)
     end
     methods(Static)
-        function [x,fval,df,dx,exitflag,iterations,funcCount] = SteepestDecent(fun,x0,maxIters,StepTolerance)
+        function [x,fval,exitflag,iterations,funcCount] = SteepestDecent(fun,x0,maxIters,StepTolerance)
             %we will use the BFGS quasi-newton method
             %good stuff: https://en.wikipedia.org/wiki/Quasi-Newton_method
 
@@ -13,23 +13,16 @@ classdef funcbund
 
             %in quasi-newton, instead of H we use A, which uses only first derivatives.
             %apprently MATLAB implements BFGS in fminunc (confirmed via documentation)
-            
-            clear global;
-            global outputFcn_global_data
 
             options = optimoptions(@fminunc,'Display','iter',...
                 'Algorithm','quasi-newton','HessUpdate','steepdesc',...
                 'MaxIterations',maxIters,...
-                'StepTolerance',StepTolerance,...
-                'OutputFcn',@outputFcn_global);
+                'StepTolerance',StepTolerance);
             [x,fval,exitflag,output] = fminunc(fun,x0,options);
             iterations = output.iterations;
             funcCount = output.funcCount;
-
-            df = fun(outputFcn_global_data(end-1).x)-fun(outputFcn_global_data(end).x);
-            dx = norm(outputFcn_global_data(end-1).x - outputFcn_global_data(end).x);
         end
-        function [x,fval,df,dx,exitflag,iterations,funcCount] = QuasiNewton(fun,x0,maxIters,StepTolerance)
+        function [x,fval,exitflag,iterations,funcCount] = QuasiNewton(fun,x0,maxIters,StepTolerance)
         %we will use the BFGS quasi-newton method
         %good stuff: https://en.wikipedia.org/wiki/Quasi-Newton_method
         
@@ -40,68 +33,36 @@ classdef funcbund
         
         %in quasi-newton, instead of H we use A, which uses only first derivatives.
         %apprently MATLAB implements BFGS in fminunc (confirmed via documentation)
-        
-            clear global;
-            global outputFcn_global_data
-
 
         options = optimoptions(@fminunc,'Display','iter','Algorithm','quasi-newton',...
             'MaxIterations',maxIters,...
-            'StepTolerance',StepTolerance,...
-            'OutputFcn',@outputFcn_global);
+            'StepTolerance',StepTolerance);
         [x,fval,exitflag,output] = fminunc(fun,x0,options);
         iterations = output.iterations;
         funcCount = output.funcCount;
-
-        df = fun(outputFcn_global_data(end-1).x)-fun(outputFcn_global_data(end).x);
-        dx = norm(outputFcn_global_data(end-1).x - outputFcn_global_data(end).x);
         end
 
         function [x,fval,exitflag,iterations,funcCount] = SimulatedAnealing(fun,x0,maxIters,FunctionTolerance)
-
-            clear global;
-            global outputFcn_global_data
-
-
             options = optimoptions('simulannealbnd','Display','iter',...
-                'MaxIterations',maxIters,'FunctionTolerance',FunctionTolerance,...
-                'OutputFcn',@outputFcn_global_sa);
+                'MaxIterations',maxIters,'FunctionTolerance',FunctionTolerance);
             
             [x,fval,exitflag,output] = simulannealbnd(fun,x0,[],[],options);
             iterations = output.iterations;
             funcCount = output.funccount;
-
-            df = fun(outputFcn_global_data(end-1).x)-fun(outputFcn_global_data(end).x);
-            dx = norm(outputFcn_global_data(end-1).x - outputFcn_global_data(end).x);
         end
 
-        function [x,fval,df,dx,exitflag,generations,funcCount] = GeneticAlgorithm(fun,x0,MaxGenerations,FunctionTolerance,Npop)
-            clear global;
-            global outputFcn_global_data
-
-
+        function [x,fval,exitflag,generations,funcCount] = GeneticAlgorithm(fun,x0,MaxGenerations,FunctionTolerance,Npop)
             options = optimoptions('ga','Display','iter',...
                 'MaxGenerations',MaxGenerations,'FunctionTolerance',FunctionTolerance,...
-                'OutputFcn',@outputFcn_global_ga,...
                 'PopulationSize',Npop);
 
             nvars = length(x0);
             [x,fval,exitflag,output,population,scores] = ga(fun,nvars,[],[],[],[],[],[],[],options);
             generations = output.generations;
             funcCount = output.funccount;
-            
-            fmin_i = mean(outputFcn_global_data(end-1).Score);
-            fmin_ip1 = mean(outputFcn_global_data(end).Score);
-            x_i = mean(outputFcn_global_data(end-1).Population);
-            x_ip1 = mean(outputFcn_global_data(end).Population);
-            df = fmin_i - fmin_ip1;
-            dx = norm(x_i-x_ip1);
         end
 
-        function [x,fval,exitflag,iterations,funcCount] = DownhillSimplex(fun,x0,maxIters,StepTolerance)
-            global outputFcn_global_data
-
-            
+        function [x,fval,exitflag,iterations,funcCount] = DownhillSimplex(fun,x0,maxIters,StepTolerance) 
             %explination on the algorithm:
             %https://www.mathworks.com/help/optim/ug/fminsearch-algorithm.html
             options = optimset('Display','iter',...
@@ -112,6 +73,34 @@ classdef funcbund
             iterations = output.iterations;
             funcCount = output.funcCount;
         end
-    
+        
+        function [A,m] = Limits2AmpAndMean(limits)
+        %limits - [xmin,xmax,ymin,ymax]
+        A = [limits(2)-limits(1),limits(4)-limits(3)];
+        m =  [mean(limits(1:2)),mean(limits(3:4))];
+        end
+
+        function plotResults(fun,Limits,T,funName)
+            fig = figure;
+            ax=axes(fig); hold(ax,'on');
+            xlabel('x'); ylabel('y'); zlabel('z');
+            fsurf(ax,fun,Limits,'EdgeColor','none');
+            axis('manual');
+            
+            methods = T.Properties.VariableNames;
+            entries = T.Variables;
+            shapes = {'o','+','*','x','s','d','p','h','^'};
+            colors = jet(length(methods));
+            for ii=1:length(methods)
+                sols = cell2mat(entries(:,ii));
+                z = fun(sols(:,1),sols(:,2)) + 1; %allow rendeer to show points above
+                scatter3(ax,sols(:,1),sols(:,2),z,50,shapes{ii},...
+                    'LineWidth',2,'MarkerEdgeColor',colors(ii,:));
+            end
+            legend([{funName},methods],'location','best');
+            colorbar(ax);
+            colormap(ax,'gray');
+
+        end
     end
 end
--- HW/HW6/main.m
+++ HW/HW6/main.m
@@ -1,31 +1,83 @@
 %% Vectorize the given functions
 fEasom = @(x) Easom(x(1),x(2));
-fEggholder = @(x) Eggholder(x(1),x(2));
 fRosenbrock = @(x) Rosenbrock(x(1),x(2));
+fEggholder = @(x) Eggholder(x(1),x(2));
 
+%settings
 maxIters = 1e4;
 StepTolerance = 1e-4;
 
-%different settings for ga and sa
-MaxGenerations = 100;
-Npop = 50; %ga
-FunctionTolerance = StepTolerance;
+%different settings for ga
+ga_MaxGenerations = 100;
+ga_Npop = 50; %ga
+ga_FunctionTolerance = 1e-4;
+
+%different settings for sa
+sa_FunctionTolerance = 1e-4;
+
+K = 5; %number of times to run each solver on each function
+M = 5; %number of methods
+
+%Table template construction
+methods = {'SteepestDecent','QuasiNewton','SimulatedAnealing','GeneticAlgorithm','DownhillSimplex'};
+VarTypes = {'cell','cell','cell','cell','cell'}; %M Times
+rowNames = {'sol1','sol2','sol3','sol4','sol5'}; %K Times
+T = table('Size',[K,M],'VariableTypes',VarTypes,...
+    'RowNames',rowNames,'VariableNames',methods);
+
+%function limits [xmin,xmax,ymin,ymax]
+EasomLimits = [0 5, 0 5];
+RosenbrockLimits = [-3 3, -1 6];
+EggholderLimits = [0 500, 0 500];
+%% Plots
+figure;
+fsurf(@Easom,EasomLimits);
+title('Easom'); xlabel('x'); ylabel('y'); zlabel('z');
+
+figure;
+fsurf(@Rosenbrock,RosenbrockLimits);
+title('Rosenbrock'); xlabel('x'); ylabel('y'); zlabel('z');
 
 
+figure;
+fsurf(@Eggholder,EggholderLimits);
+title('Eggholder'); xlabel('x'); ylabel('y'); zlabel('z');
+%% Easom
+[A,m] = funcbund.Limits2AmpAndMean(EasomLimits);
+x0 = (rand(K,2)-0.5).*A+m;
+T_Easom = T;
+for ii=1:K
+[T_Easom.SteepestDecent{ii},fval,exitflag,iterations,funcCount] = funcbund.SteepestDecent(fEasom,x0(ii,:),maxIters,StepTolerance);
+[T_Easom.QuasiNewton{ii},fval,exitflag,iterations,funcCount]  = funcbund.QuasiNewton(fEasom,x0(ii,:),maxIters,StepTolerance);
+[T_Easom.SimulatedAnealing{ii},fval,exitflag,iterations,funcCount] = funcbund.SimulatedAnealing(fEasom,x0(ii,:),maxIters,sa_FunctionTolerance);
+[T_Easom.GeneticAlgorithm{ii},fval,exitflag,generations,funcCount] = funcbund.GeneticAlgorithm(fEasom,x0(ii,:),ga_MaxGenerations,ga_FunctionTolerance,ga_Npop);
+[T_Easom.DownhillSimplex{ii},fval,exitflag,iterations,funcCount] = funcbund.DownhillSimplex(fEasom,x0(ii,:),maxIters,StepTolerance);
+end
 
+funcbund.plotResults(@Easom,EasomLimits,T_Easom,'Easom');
+%% Rosenbrock
+[A,m] = funcbund.Limits2AmpAndMean(RosenbrockLimits);
+x0 = (rand(K,2)-0.5).*A+m;
+T_Rosenbrock = T;
+for ii=1:K
+[T_Rosenbrock.SteepestDecent{ii},fval,exitflag,iterations,funcCount] = funcbund.SteepestDecent(fRosenbrock,x0(ii,:),maxIters,StepTolerance);
+[T_Rosenbrock.QuasiNewton{ii},fval,exitflag,iterations,funcCount]  = funcbund.QuasiNewton(fRosenbrock,x0(ii,:),maxIters,StepTolerance);
+[T_Rosenbrock.SimulatedAnealing{ii},fval,exitflag,iterations,funcCount] = funcbund.SimulatedAnealing(fRosenbrock,x0(ii,:),maxIters,sa_FunctionTolerance);
+[T_Rosenbrock.GeneticAlgorithm{ii},fval,exitflag,generations,funcCount] = funcbund.GeneticAlgorithm(fRosenbrock,x0(ii,:),ga_MaxGenerations,ga_FunctionTolerance,ga_Npop);
+[T_Rosenbrock.DownhillSimplex{ii},fval,exitflag,iterations,funcCount] = funcbund.DownhillSimplex(fRosenbrock,x0(ii,:),maxIters,StepTolerance);
+end
 
+funcbund.plotResults(@Rosenbrock,RosenbrockLimits,T_Rosenbrock,'Rosenbrock');
+%% EggHolder
+[A,m] = funcbund.Limits2AmpAndMean(EggholderLimits);
+x0 = (rand(K,2)-0.5).*A+m;
+T_EggHolder = T;
+for ii=1:K
+[T_EggHolder.SteepestDecent{ii},fval,exitflag,iterations,funcCount] = funcbund.SteepestDecent(fEggholder,x0(ii,:),maxIters,StepTolerance);
+[T_EggHolder.QuasiNewton{ii},fval,exitflag,iterations,funcCount]  = funcbund.QuasiNewton(fEggholder,x0(ii,:),maxIters,StepTolerance);
+[T_EggHolder.SimulatedAnealing{ii},fval,exitflag,iterations,funcCount] = funcbund.SimulatedAnealing(fEggholder,x0(ii,:),maxIters,sa_FunctionTolerance);
+[T_EggHolder.GeneticAlgorithm{ii},fval,exitflag,generations,funcCount] = funcbund.GeneticAlgorithm(fEggholder,x0(ii,:),ga_MaxGenerations,ga_FunctionTolerance,ga_Npop);
+[T_EggHolder.DownhillSimplex{ii},fval,exitflag,iterations,funcCount] = funcbund.DownhillSimplex(fEggholder,x0(ii,:),maxIters,StepTolerance);
+end
 
-%% Steepest Decent
-[x,fval,df,dx,exitflag,iterations,funcCount] = funcbund.SteepestDecent(fEasom,[2,2],maxIters,StepTolerance);
-
-%% Quasi-Newton
-[x,fval,df,dx,exitflag,iterations,funcCount]  = funcbund.QuasiNewton(fEasom,[2,2],maxIters,StepTolerance);
-
-%% Simulated annealing
-[x,fval,df,dx,exitflag,iterations,funcCount] = funcbund.SimulatedAnealing(fEasom,[2,2],maxIters,FunctionTolerance);
-
-%% Genetic Algorithm
-[x,fval,df,dx,exitflag,generations,funcCount] = funcbund.GeneticAlgorithm(fEasom,[2,2],MaxGenerations,FunctionTolerance,Npop);
-
-%% Downhill simplex
-[x,fval,exitflag,iterations,funcCount] = funcbund.DownhillSimplex(fEasom,[2,2],maxIters,StepTolerance);
+funcbund.plotResults(@Eggholder,EggholderLimits,T_EggHolder,'Eggholder');
